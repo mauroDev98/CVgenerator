d9265188246b2f8345f48221fedea49a0928ed09
mauroDev98 <mauroriverofranco@gmail.com>
2026-04-08 12:51:13 -0600

    agregado panel languages con boton y lista dinamica

--- src/VentanaCV.java
+++ src/VentanaCV.java
@@ -3,16 +3,16 @@ import java.awt.*;
 
 class VentanaCV extends JFrame {
     VentanaCV() {
-        setTitle("Generador de CV");
+        setTitle("CV generator");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panelPersonal = new JPanel();
-        panelPersonal.add(new JLabel("Nombre:"));
+        panelPersonal.add(new JLabel("Name:"));
         panelPersonal.add(new JTextField(20));
-        panelPersonal.add(new JLabel("Correo:"));
+        panelPersonal.add(new JLabel("Mail:"));
         panelPersonal.add(new JTextField(20));
-        panelPersonal.add(new JLabel("Teléfono:"));
+        panelPersonal.add(new JLabel("Telephone:"));
         panelPersonal.add(new JTextField(20));
 
         panelPersonal.setLayout(new GridLayout(3, 2, 10, 10));
@@ -20,11 +20,42 @@ class VentanaCV extends JFrame {
         JPanel wrapperPersonal = new JPanel();
         wrapperPersonal.add(panelPersonal);
 
+        JTextField campoLanguage = new JTextField(20);
+        JTextField campoLevel = new JTextField(20);
+        JTextField campoCertification = new JTextField(20);
+
+        DefaultListModel<String> listaModel = new DefaultListModel<>();
+        JList<String> listaIdiomas = new JList<>(listaModel);
+        JButton botonAgregar = new JButton("Add language");
+
+        botonAgregar.addActionListener(e -> {
+            String idioma = campoLanguage.getText() + " - " + campoLevel.getText() + " - " + campoCertification.getText();
+            listaModel.addElement(idioma);
+            campoLanguage.setText("");
+            campoLevel.setText("");
+            campoCertification.setText("");
+        });
+
+        JPanel panelIdiomas = new JPanel();
+        panelIdiomas.add(new JLabel("Language:"));
+        panelIdiomas.add(campoLanguage);
+        panelIdiomas.add(new JLabel("Level:"));
+        panelIdiomas.add(campoLevel);
+        panelIdiomas.add(new JLabel("Certification:"));
+        panelIdiomas.add(campoCertification);
+
+        panelIdiomas.setLayout(new GridLayout(3, 2, 10, 10));
+
+        JPanel wrapperIdiomas = new JPanel();
+        wrapperIdiomas.add(panelIdiomas);
+        wrapperIdiomas.add(botonAgregar);
+        wrapperIdiomas.add(new JScrollPane(listaIdiomas));
+
         JTabbedPane pestanas = new JTabbedPane();
-        pestanas.addTab("Datos Personales", wrapperPersonal);
-        pestanas.addTab("Idiomas", new JPanel());
-        pestanas.addTab("Experiencia", new JPanel());
-        pestanas.addTab("Educación", new JPanel());
+        pestanas.addTab("Personal Data", wrapperPersonal);
+        pestanas.addTab("Languages", wrapperIdiomas);
+        pestanas.addTab("Experience", new JPanel());
+        pestanas.addTab("Education", new JPanel());
         add(pestanas);
 
         setVisible(true);
