987b866a7beef89703b99988143191aeb455ca30
mauroDev98 <mauroriverofranco@gmail.com>
2026-04-08 11:57:52 -0600

    agregado layout de datos personales

--- src/VentanaCV.java
+++ src/VentanaCV.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 class VentanaCV extends JFrame {
     VentanaCV() {
@@ -14,8 +15,13 @@ class VentanaCV extends JFrame {
         panelPersonal.add(new JLabel("Teléfono:"));
         panelPersonal.add(new JTextField(20));
 
+        panelPersonal.setLayout(new GridLayout(3, 2, 10, 10));
+
+        JPanel wrapperPersonal = new JPanel();
+        wrapperPersonal.add(panelPersonal);
+
         JTabbedPane pestanas = new JTabbedPane();
-        pestanas.addTab("Datos Personales", panelPersonal);
+        pestanas.addTab("Datos Personales", wrapperPersonal);
         pestanas.addTab("Idiomas", new JPanel());
         pestanas.addTab("Experiencia", new JPanel());
         pestanas.addTab("Educación", new JPanel());
